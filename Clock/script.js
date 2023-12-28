38b896f7960638c8233a5b997335a190b50a327c
HuseynliTogrul <toghrulih@code.edu.az>
2023-12-29 00:57:03 +0400

    clock task end

---
+++ Clock/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const secondPointer = document.getElementById("second");
+const minutePointer = document.getElementById("minute");
+const hourPointer = document.getElementById("hour");
+
+const secondText = document.getElementById("d__second");
+const minuteText = document.getElementById("d__minute");
+const hourText = document.getElementById("d__hour");
+
+
+const clock = () => {
+    const time = new Date();
+    hourText.textContent = String(time.getHours()).padStart(2, 0);
+    minuteText.textContent = String(time.getMinutes()).padStart(2, 0);
+    secondText.textContent = String(time.getSeconds()).padStart(2, 0);
+
+    hourPointer.style.transform = `rotate(${(time.getHours() % 12) * 30 + time.getMinutes() / 2}deg)`
+    minutePointer.style.transform = `rotate(${(time.getMinutes() * 6)}deg)`
+    secondPointer.style.transform = `rotate(${(time.getSeconds() * 6)}deg)`
+}
+
+setInterval(clock, 1000)
